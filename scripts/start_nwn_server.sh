7a4594974ae7ca3be43ceb875d8017d4ddfe4e3f
bartleby294 <bartleby12804@gmail.com>
2020-06-06 17:36:04 -0400

    Auto build scripts added

---
+++ scripts/start_nwn_server.sh
@@ -0,0 +1,3 @@
+cd ../../nwnserver/bin/linux-x86
+./nwserver-linux -module alfa_050_moonshaes -nwsyncurl https://www.alfanwn1.org/nwsync/ -port 5555 -maxclients 64 -servervault 0 -dmpassword computeStick -adminpassword computeStick -elc 0 -pvp 2 -pauseandplay 0 -ilr 0 -difficulty 3 -servername 'ALFA - Moonshaes - Beta' -oneparty 0
+
